79abff0c00982a68003e80d05158236c883f44e3
JiarouDeng <dpj5243@psu.edu>
2020-09-19 02:10:22 +0000

    change %c to %d

--- main.c
+++ main.c
@@ -7,14 +7,14 @@ int digit_sum(int n);
 int main(void){
   char*n = readline("Enter an int: ");
   int c = atof(n);
-  int sum1 = digit_sum(c);
-  printf("sum of digits of %d is %c.\n",c,sum1);
+  int sum = digit_sum(c);
+  printf("sum of digits of %d is %d.\n",c,sum);
 }
 
 
 int digit_sum(int n){
   if (n>0){
-    return (n%10 +digit_sum(n/10));
+    return (n%10 + digit_sum(n/10));
    }
   else{
     return (n);
